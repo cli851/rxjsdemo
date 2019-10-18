5e71620c681f417949e43b02077e9f435d4e8ab4
cli851 <can_li@comcast.com>
2019-10-18 13:52:07 -0400

    init

---
+++ src/app/ts/ts.component.ts
@@ -0,0 +1,28 @@
+import {Component, OnInit, ViewChild, ElementRef} from '@angular/core';
+import * as Rx from 'rxjs';
+import 'rxjs-compat/add/observable/fromEvent';
+import 'rxjs-compat/add/operator/map';
+
+
+
+@Component({
+  selector: 'app-ts',
+  templateUrl: './ts.component.html',
+  styleUrls: ['./ts.component.css']
+})
+export class TsComponent implements OnInit {
+  @ViewChild('firstNameInput') todo: ElementRef;
+  constructor() {
+
+  }
+
+  ngOnInit() {
+    let todo = document.getElementById('todo');
+    let input$ = Rx.Observable.fromEvent(todo, 'keyup');
+    input$
+      .map(ev=>ev.target.value)
+      .subscribe(value=>console.log(value));
+  }
+
+
+}
